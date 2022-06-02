b0332127ad3abe509d2c4ce9d0f9e218615f766f
saulRM10 <43281985+saulRM10@users.noreply.github.com>
2022-06-02 14:56:26 -0700

    create useState hook to deal with user object

---
+++ Verify.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+
+
+// store user info; username, repo 
+const [user, setUser] = useState({
+    username : '',
+    repo : ''
+}); 
+
+function Verify(){
+    return (
+        <></>
+    )
+};
+
+export default Verify; 
